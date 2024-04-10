a63412fea12d885c59f4563ae0cf04bd78251c1d
pksh-23514 <prabhat23514k@gmail.com>
2024-04-10 21:45:40 +0530

    Methods in Python

---
+++ chronicles/methods.py
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+"""
+**************************************************************
+**************************************************************
+"""
+# Summary:
+# ========
+# Methods in Python is similar to a function, except it is associated with Objects/Classes.
+# It is called by its name but it is associated to an Object (dependent) i.e. the reference of the Class in which it is defined is invoked.
+# It's definition always includes 'self' as its first parameter.
+# It is implicitly passed to the Object on which it is invoked.
+# It may or may not return any data.
+# It can operate on the data (instance variables) that is contained by the corresponding Class.
+# It can alter the Object's state.
+
+# Syntax:
+# class Class_Name
+#	def Method_Name () :
+#		......
+#		Method Body
+#		......
+
+# Methods can be User-defined or In-Built.
+# 
