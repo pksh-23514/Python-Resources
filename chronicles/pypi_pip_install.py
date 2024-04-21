90f98d109b2c2b564d92a2d1aba12b3cae3f43a8
pksh-23514 <prabhat23514k@gmail.com>
2024-04-21 13:35:36 +0530

    PyPi and pip in Python

---
+++ chronicles/pypi_pip_install.py
@@ -0,0 +1,25 @@
+"""
+**************************************************************
+**************************************************************
+"""
+# Summary:
+# ========
+# PyPi is the acronym for Python Package Index. It is a repository for Open-Source third-party Python packages.
+# It is a standard system where developers can publish and distribute their Python packages.
+# The libraries that come internally with Python are the Standard or Built-in Libraries.
+# There are many other libraries available that are open-sourced and shared on PyPi.
+# Developers create and distribute packages in a specific format called “distributions” which contain all the files needed to install and run their software.
+# The "pip install" command can be used to install these packages on the system.
+# The 'pip' is a package manager for Python that is used to install and manage software packages written in Python.
+# The "pip install" command needs to be run on the Command Line Prompt.
+# Syntax: pip install <package-name>
+# Where: 'package-name' is the name of the Package that needs to be installed.
+
+# Eg.- If there is a need to work with Excel, there are certain packages handy to operate on the Excel files.
+# Steps:
+# 1. In the Google, search for the "Python packages for Excel".
+# 2. The top results will show the list of packages available.
+# 3. There shall be a Documentation link for the particular package which explains about the complete package.
+# 4. There shall be a Download link which will take you to the official "Python Software Foundation" page for the package.
+# 5. In your system, on the Command Line: pip install openopyxl
+# 6. The package is installed and ready to be imported in your program.
