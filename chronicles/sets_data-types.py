e3e7e22afd67edae68903ef795243b8433eb7d6b
pksh-23514 <prabhat23514k@gmail.com>
2024-07-16 22:58:23 +0530

    Create Set from other Data-types in Python

--- chronicles/sets_data-types.py
+++ chronicles/sets_data-types.py
@@ -5,6 +5,16 @@ my_list = [1, 1, 1, 1, 1, 2, 2, 2, 3, 3, 3, 3, 3, 4]
 my_set = set (my_list)
 print ("DATA: ", my_set)
 
+my_str = "ABCDE"
+my_list = ['A', 'B', 'C', 'D', 'E']
+my_tuple = ('A', 'B', 'C', 'D', 'E')
+my_dict = {'A': 1, 'B': 2, 'C': 3, 'D': 4, 'E': 5}
+print ("STRING TO SET           : ", set (my_str))
+print ("LIST TO SET             : ", set (my_list))
+print ("TUPLE TO SET            : ", set (my_tuple))
+print ("DICTIONARY TO SET       : ", set (my_dict))
+print ("DICTIONARY ITEMS TO SET : ", set (my_dict.items ()))
+
 my_set = {1, 2, 3}
 print ("DATA: ", my_set)
 print ("LENGTH: ", len (my_set))
@@ -28,10 +38,13 @@ print ("LENGTH: ", len (my_set))
 # They cannot be sorted, indexed or sliced.
 # It uses '{}' brackets and ',' commas to separate the Objects present in the Sets.
 # They are Immutable i.e. once the element is assigned inside a Set, it cannot be changed. However, elements can be added or removed in the Sets.
-# The "set()" function can be used to create a new Set.
 # It is very useful to pick the unique elements from a pile of duplicate elements List using the "set()" function.
 # The "len()" function can be used to find the Length of the Sets.
 
+# The "set()" function can be used to create a new Set.
+# Syntax: set (iterable)
+# Where: iterable - Object that can be a sequence (string/lists) or collection (sets/dictionary) or any iterator.
+# If no parameter is passed, the function will return a Set with 0 elements.
+
 # The "add()" method can be used to add a new element to the Sets. However, it will not add the element if it is already existing the in the Set.
 # Note: The values (True, 1) and (False, 0) are considered the same value in sets and are treated as duplicates.
-
