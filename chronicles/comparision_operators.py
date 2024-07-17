8900cf5c5dbfc80b0303bc9eb68d740272fa5b27
pksh-23514 <prabhat23514k@gmail.com>
2024-07-17 20:29:34 +0530

    Comparision of same Int and Float values

--- chronicles/comparision_operators.py
+++ chronicles/comparision_operators.py
@@ -7,6 +7,9 @@ print (5 > 23)
 print (1 <= 0)
 print ("hello" <= 'hi')
 
+print (2.0 != 2)
+print (4**0.5 == 2)
+
 """
 **************************************************************
 **************************************************************
@@ -22,3 +25,5 @@ print ("hello" <= 'hi')
 # Greater than or equal to - >=
 # Less than or equal to - <=
 
+# In Python, the value and it's decimal form is equal.
+# Eg. - The integer 3 and the floating-point number 3.0 clearly have the same value, so if (3 == 3.0) is true.
